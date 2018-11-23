5737fb847f1948a9c94b2c5d7ff91ab6de229ee2
ldd <2282703625@qq.com>
2018-11-23 19:38:44 +0800

    v1.0

---
+++ src/main/java/com/baizhi/managesystemone/ManagesystemoneApplication.java
@@ -0,0 +1,12 @@
+package com.baizhi.managesystemone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagesystemoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagesystemoneApplication.class, args);
+    }
+}
